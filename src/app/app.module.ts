921dd48f0c1b6447b58153b0042cf870c06458c7
Pliski Pliski <pliski@gmail.com>
2017-01-14 18:01:59 +0100

    rename: profile >> sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,17 @@ import { StoreModule } from '@ngrx/store';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BuddyProfileComponent } from './buddy-profile/buddy-profile.component';
-import { BuddyStoreComponent } from './buddy-store/buddy-store.component';
+import { BuddySidebarComponent } from './view/buddy-sidebar/buddy-sidebar.component';
+import { BuddyStoreComponent } from './view/buddy-store/buddy-store.component';
 import { ModelModule } from './model/model.module';
 
 import { TestService } from './shared/test.service';
-import { BuddyItemComponent } from './buddy-store/buddy-item/buddy-item.component';
+import { BuddyItemComponent } from './view/buddy-store/buddy-item/buddy-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BuddyProfileComponent,
+    BuddySidebarComponent,
     BuddyStoreComponent,
     BuddyItemComponent
   ],
